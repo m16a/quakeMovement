11dfc7b5334c423347cf32259727a6bc3773f0c6
m16a <mshvorak@gmail.com>
2017-03-29 11:00:56 +0300

    \!U client message definition + messages was moved to shared header

--- shared.cpp
+++ shared.cpp
@@ -1,6 +1,10 @@
 #include "shared.h"
-#include <ctime>
 #include <iostream>
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
 
 const int kServerPort =  60005;
 const char* kHost = "127.0.0.1";
@@ -22,3 +26,40 @@ unsigned char GetPacketIdentifier(RakNet::Packet *p)
 		return (unsigned char) p->data[0];
 }
 
+void Dump(const pstate& s)
+{
+	std::cout << "pstate t:" << s.lastCommandTime << " (" << s.pos[0] << ", " << s.pos[1] << ", " << s.pos[2] << ")\n";
+}
+
+void FillMsg(SvMsg& m)
+{
+	m.useTimeStamp = ID_TIMESTAMP;
+	m.serverTime = RakNet::GetTime();
+	m.typeId = ID_SV_MSG;
+	m.forward = 0;
+	m.right = 0;
+	m.up = 0;
+}
+
+
+void Dump(SvMsg& m)
+{
+	std::cout << "t:" << m.serverTime << "(" << (int)m.forward << " " << (int)m.right << " " << (int)m.up << ")\n";
+	/*
+	for (int i=0; i < sizeof(Msg); ++i)
+	{
+		std::cout << " " << std::hex << std::setfill('0') << std::setw(2) << *(reinterpret_cast<unsigned char*>(&m)+i);
+	}
+	std::cout << "\n";
+	*/
+}
+
+void ReverseTimeStamp(SvMsg& m)
+{
+	RakNet::BitStream::ReverseBytesInPlace(reinterpret_cast<unsigned char*>(&m.serverTime), sizeof(RakNet::Time));
+}
+
+void Dump(ClMsg& m)
+{
+	Dump(m.state);
+}
--- shared.h
+++ shared.h
@@ -4,8 +4,14 @@
 #include "raknet/RakPeerInterface.h"
 #include "raknet/RakNetTypes.h"
 #include "raknet/MessageIdentifiers.h"
+#include "raknet/GetTime.h"
+#include "raknet/BitStream.h"
 
-#include "message.h"
+enum 
+{
+	ID_SV_MSG = ID_USER_PACKET_ENUM+1
+
+};
 
 #define USE_BIT_STREAM 0
 
@@ -28,5 +34,38 @@ struct pstate
 	float pos[3];
 };
 
+void Dump(const pstate& s);
+
+//message from client to server
+#pragma pack(push, 1)
+struct SvMsg 
+{
+	unsigned char useTimeStamp; // Assign ID_TIMESTAMP to this
+	RakNet::Time serverTime; // Put the system time in here returned by RakNet::GetTime()
+	unsigned char typeId; // You should put here an enum you defined after the last one defined in MessageIdentifiers.h, lets say ID_SET_TIMED_MINE
+	signed char forward, right, up;
+
+	//RakNet::NetworkID networkId; // NetworkID of the mine, used as a common method to refer to the mine on different computers
+	//RakNet::SystemAddress systemAddress; // The SystenAddress of the player that owns the mine
+};
+#pragma pack(pop)
+
+void FillMsg(SvMsg& m);
+void Dump(SvMsg& m);
+void ReverseTimeStamp(SvMsg& m);
+
+
+//message from server to client
+#pragma pack(push, 1)
+struct ClMsg 
+{
+	unsigned char typeId; 
+	pstate state;
+	//RakNet::NetworkID networkId; // NetworkID of the mine, used as a common method to refer to the mine on different computers
+	//RakNet::SystemAddress systemAddress; // The SystenAddress of the player that owns the mine
+};
+#pragma pack(pop)
+
+void Dump(ClMsg& m);
 #endif //_SHARED_H_
 
